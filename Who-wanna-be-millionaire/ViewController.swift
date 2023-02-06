54dec07f4dcfeeea0d51b4b01f7539e941f25bc9
SiLVERSHTAiN <silvershtain@protonmail.com>
2023-02-07 00:48:04 +0700

    Initial Commit

---
+++ Who-wanna-be-millionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Who-wanna-be-millionaire
+//
+//  Created by Aleksandr on 07.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
