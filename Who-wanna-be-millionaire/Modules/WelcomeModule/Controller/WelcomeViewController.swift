b7253640fd64031e21e9618f464ef8b699945183
Leonid <78638629+theheleo@users.noreply.github.com>
2023-02-08 00:42:43 +0300

    WelcomeView is finished

--- Who-wanna-be-millionaire/Modules/WelcomeModule/Controller/WelcomeViewController.swift
+++ Who-wanna-be-millionaire/Modules/WelcomeModule/Controller/WelcomeViewController.swift
@@ -1,35 +1,57 @@
-//
-//  WelcomeViewController.swift
-//  Who-wanna-be-millionaire
-//
-//  Created by Aleksandr on 07.02.2023.
-//
-
 import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    let welcomeView = WelcomeView()
+    private lazy var backgroundImage: UIImageView = {
+        let background = UIImageView(frame: .zero)
+        let image = UIImage(named: WelcomeConstants.backgroundImageName)
+        background.image = image
+        background.contentMode = .scaleAspectFill
+        background.translatesAutoresizingMaskIntoConstraints = false
+        return background
+    }()
+    
+    private lazy var logoAndLabelView = LogoAndLabelView()
+    private lazy var buttonsView = ButtonsView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        view.addSubview(welcomeView)
-        configure()
+
+        setupViews()
+        setConstraints()
     }
     
+    func setupViews() {
+        
+        view.addSubview(backgroundImage)
+        
+        view.addSubview(logoAndLabelView)
+        logoAndLabelView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(buttonsView)
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
 }
 
 extension WelcomeViewController {
     
-    func configure(){
-        
+    func setConstraints(){
         NSLayoutConstraint.activate([
-            welcomeView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
-            welcomeView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
-            welcomeView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            welcomeView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
+            logoAndLabelView.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            logoAndLabelView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            logoAndLabelView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            logoAndLabelView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.46),
+            
+            buttonsView.topAnchor.constraint(equalTo: logoAndLabelView.bottomAnchor, constant: 107),
+            buttonsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            buttonsView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            buttonsView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.27)
         ])
     }
 }
---
+++ Who-wanna-be-millionaire/Modules/WelcomeModule/Model/WelcomeConstants.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+enum WelcomeConstants {
+    static let backgroundImageName = "background"
+    static let welcomeMillionaireLogo = "big_logo"
+    static let welcomLeabelText = "Welcome"
+    static let gameNameLabelText = "to Who Wants to be a Millionare"
+    static let textFont = "Avenir Next"
+    static let textFontBold = "Avenir Next Bold"
+    static let roolsButtonTitle = "Правила игры"
+    static let startButtonTitle = "Начать игру"
+}
