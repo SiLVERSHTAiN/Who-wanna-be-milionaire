5abe45bdd986942777092d0b3566fdfe1bda74b8
SiLVERSHTAiN <silvershtain@protonmail.com>
2023-02-08 21:54:36 +0700

    Start setup view

---
+++ Who-wanna-be-millionaire/Modules/NickNameModule/Model/NickNameModel.swift
@@ -0,0 +1,35 @@
+//
+//  NickNameModel.swift
+//  Who-wanna-be-millionaire
+//
+//  Created by Aleksandr on 08.02.2023.
+//
+
+import UIKit
+
+enum Resources {
+    
+    enum Images {
+        static let background = UIImage(named: "background_gold")
+        static let gameLogo = UIImage(named: "medium_logo")
+    }
+    
+    enum Texts {
+        static let buttonText = "Register"
+        static let mainText = "Enter your nickname"
+    }
+    
+    enum Colors {
+        static let buttonColorGray = UIColor(hexString: "#95D5E3")
+    }
+    
+    enum Fonts: String {
+        case textAvenirFont = "Avenir Next"
+        case textAvenirFontBold = "Avenir Next Bold"
+        
+        static func setFont(name: Resources.Fonts, size: CGFloat) -> UIFont {
+            UIFont(name: name.rawValue, size: size) ?? UIFont()
+        }
+    }
+    
+}
--- Who-wanna-be-millionaire/Modules/NickNameModule/View/NickNameView.swift
+++ Who-wanna-be-millionaire/Modules/NickNameModule/View/NickNameView.swift
@@ -20,12 +20,54 @@ final class NickNameView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private lazy var backgroundImage: UIImageView = {
+        let background = UIImageView()
+        background.image = Resources.Images.background
+        return background
+    }()
+    
+    private lazy var mainLogoImage: UIImageView = {
+        let logo = UIImageView()
+        logo.image = Resources.Images.gameLogo
+        return logo
+    }()
+    
+    private lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.text = Resources.Texts.mainText
+        label.textAlignment = .center
+        label.font = Resources.Fonts.setFont(name: .textAvenirFont, size: 28)
+        return label
+    }()
+    
+    private lazy var nickName: UITextField = {
+        let name = UITextField()
+        return name
+    }()
+    
+    private lazy var registerButton: UIButton = {
+        let register = UIButton()
+        register.setTitle(Resources.Texts.buttonText, for: .normal)
+        register.makeSystem(register)
+        return register
+    }()
+    
+//    private lazy var stackView: UIStackView = {
+//        let stack = UIStackView()
+//        stack.axis = .vertical
+//        stack.distribution = .fill
+//        stack.spacing = 10
+//        return stack
+//    }()
+    
+    private var stackView = UIStackView(.stack
 }
 
 extension NickNameView {
     
     func setupViews() {
-        
+        setupView(stackView)
     }
     
     func constraintViews () {
@@ -33,6 +75,7 @@ extension NickNameView {
     }
     
     func configureAppearance() {
+        
         backgroundColor = .red
     }
 }
